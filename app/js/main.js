e9ea210bb43ecc3a2c00e012c2a25ac2be537175
Ivan Prosyanik <vanyaprosyanik89@gmail.com>
2023-03-12 03:08:34 +0200

    remade full homework #12

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,44 @@
-$(function() {
-
-
-
-  var mixer = mixitup('.category-cards', {
-    load: {
-      filter: '.category-a'
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector('.header');
+  window.onscroll = () => {
+    if (window.pageYOffset > 100) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
     }
+  };
+
+  const filterBtns = document.querySelectorAll(".categories-nav__btn");
+  const grid = document.querySelector(".categories-list");
+
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      // remove active class from all buttons
+      filterBtns.forEach((filterBtn) => {
+        filterBtn.classList.remove("active");
+      });
+      // add active class to clicked button
+      btn.classList.add("active");
+
+      // get the filter value from the clicked button
+      const filterValue = btn.getAttribute("data-filter");
+
+      // filter the grid items based on the filter value
+      for (const item of grid.children) {
+        if (filterValue === "all") {
+          // item.style.display = "block";
+          item.classList.remove('hide');
+          item.classList.add('show');
+        } else if (item.classList.contains(filterValue)) {
+          // item.style.display = "block";
+          item.classList.remove('hide')
+          item.classList.add('show')
+        } else {
+          // item.style.display = "none";
+          item.classList.remove('show')
+          item.classList.add('hide')
+        }
+      }
+    });
   });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,9 @@ const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
-const svgSprite = require('gulp-svg-sprite');
 const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
+const ttf2woff2 = require('gulp-ttf2woff2');
 
 function browsersync() {
   browserSync.init({
@@ -20,43 +21,40 @@ function browsersync() {
 
 function styles() {
   return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
-    .pipe(concat('style.min.css'))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 10 versions'],
-      grid: true
-    }))
+    .pipe(scss())
+    .pipe(concat('style.css'))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 10 versions'],
+        grid: true
+      })
+    )
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
 }
 
-function scripts() {
-  return src([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/mixitup/dist/mixitup.min.js',
-    'app/js/main.js'
-  ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
-}
+// function scripts() {
+//   return src(["app/js/main.js"])
+//   .pipe(concat('main.js'))
+//   .pipe(dest('app/js'))
+//   .pipe(browserSync.stream())
+// }
 
-function images() {
-  return src('app/images/**/*.*')
-  .pipe(imagemin([
-    imagemin.gifsicle({ interlaced: true }),
-    imagemin.mozjpeg({ quality: 75, progressive: true }),
-    imagemin.optipng({ optimizationLevel: 5 }),
-    imagemin.svgo({
-      plugins: [
-        { removeViewBox: true },
-        { cleanupIDs: false }
-      ]
-    })
-  ]))
-  .pipe(dest('dist/images'))
-}
+// function images() {
+//   return src('app/images/**/*.*')
+//   .pipe(imagemin([
+//     imagemin.gifsicle({ interlaced: true }),
+//     imagemin.mozjpeg({ quality: 75, progressive: true }),
+//     imagemin.optipng({ optimizationLevel: 5 }),
+//     imagemin.svgo({
+//       plugins: [
+//         { removeViewBox: true },
+//         { cleanupIDs: false }
+//       ]
+//     })
+//   ]))
+//   .pipe(dest('dist/images'))
+// }
 
 function svgSprites() {
   return src('app/images/icons/*.svg') // выбираем в папке с иконками все файлы с расширением svg
@@ -72,14 +70,20 @@ function svgSprites() {
     .pipe(dest('app/images')); // указываем, в какую папку поместить готовый файл спрайта
 }
 
-function build() {
-  return src([
-    'app/**/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js'
-  ], {base: 'app'})
-  .pipe(dest('dist'))
+function convertFonts() {
+  return src('app/fonts/*.ttf')
+    .pipe(ttf2woff2())
+    .pipe(dest('app/fonts'));
 }
+
+// function build() {
+//   return src([
+//     'app/**/*.html',
+//     'app/css/style.min.css',
+//     'app/js/main.min.js'
+//   ], {base: 'app'})
+//   .pipe(dest('dist'))
+// }
 
 function cleanDist() {
   return del('dist')
@@ -87,18 +91,19 @@ function cleanDist() {
 
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/**/*.html']).on('change', browserSync.reload);
+  // watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/images/icons/*.svg'], svgSprites);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
-exports.scripts = scripts;
+// exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
-exports.images = images;
+// exports.images = images;
 exports.cleanDist = cleanDist;
 exports.svgSprites = svgSprites;
-exports.build = series(cleanDist, images, build);
+exports.convertFonts = convertFonts;
+// exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(svgSprites, styles, scripts, browsersync, watching);
+exports.default = parallel(svgSprites, styles, browsersync, watching);
